0efb0c97bb18d44f34e887a63c74018cddf2cb5a
Tony Sharples <tonysharples22@me.com>
2024-01-22 17:19:39 +0000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tony Sharples on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
