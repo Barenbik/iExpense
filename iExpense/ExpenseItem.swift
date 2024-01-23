63677eb5a9b6f3f1057d6f336d5e5ccd719edce5
Tony Sharples <tonysharples22@me.com>
2024-01-23 18:47:43 +0000

    Create an ExpenseItem struct

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Tony Sharples on 23/01/2024.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
---
+++ iExpense/Expenses.swift
@@ -0,0 +1,8 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Tony Sharples on 23/01/2024.
+//
+
+import Foundation
